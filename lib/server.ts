f62cf4d9d04dd8e3f626a455c84714ecec2f33b3
oswaldo valdez <oswaldovaldez92@gmail.com>
2025-09-22 10:51:27 -0600

    Update server.ts
    asdsa

--- lib/server.ts
+++ lib/server.ts
@@ -92,7 +92,7 @@ res.setHeader("Access-Control-Allow-Origin", "*");
 res.setHeader("Access-Control-Allow-Credentials", "true");
 res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
 //res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
-    res.setHeader('Access-Control-Allow-Headers', 'authorization, content-type');
+    res.setHeader("Access-Control-Allow-Headers", "*");
      // ✅ Preflight (OPTIONS)
     if (req.method === "OPTIONS") {
       console.log("OPTIONS");
